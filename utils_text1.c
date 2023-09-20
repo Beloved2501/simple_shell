2917e43efbe10d987c8f96cadb16824ade5c2e23
samwelmakokha <samwelmakokha10@gmail.com>
2023-09-20 14:50:27 -0700

    update file

--- utils_text1.c
+++ utils_text1.c
@@ -98,12 +98,14 @@ char *_strdup(char *str)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	while (s1[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
+
+		i++;
 	}
 
 	return (0);
